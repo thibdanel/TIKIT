526d166f01da4fb87ca9d4ce740d3f757bef1162
Thibault Danel <thibdanel@hotmail.fr>
2022-03-02 12:37:30 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :services
+    # resources :user_service, only: [:new, :create]
+
+  resources :user_services, only: [:destroy, :update, :edit] do
+    resources :tasks, only: [:new, :create ]
+  end
+
+  resources :tasks, only: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
